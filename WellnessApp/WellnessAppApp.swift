a68009b33be791d85569ac7e1096990e933af4b8
ooney <62164814+ooneykabooey@users.noreply.github.com>
2026-07-30 03:57:31 -0500

    Initial Commit

---
+++ WellnessApp/WellnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessAppApp.swift
+//  WellnessApp
+//
+//  Created by Levi Daniel on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct WellnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
